f19087bc099c03da9a3f83ab0731ac91444bc10f
julian-evers <133691040+julian-evers@users.noreply.github.com>
2023-10-13 02:41:37 +0200

    Update
    update notebook
    structural changes

--- pybamm_tea/__init__.py
+++ pybamm_tea/__init__.py
@@ -4,6 +4,10 @@ Analysis of battery cells, built on top of the Python Battery Mathematical
 Modelling library.
 """
 
+<<<<<<< Updated upstream
 __version__ = "0.0.0"
 
 from .tea import TEA
+=======
+__version__ = "0.0.0"
+>>>>>>> Stashed changes
